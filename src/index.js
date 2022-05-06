9b8c53ee158aa2f7cc1b109cfbe89112e04e86e5
sabbirpboss <sabbir.ah799@gmail.com>
2022-05-06 11:31:30 +0600

    react version 18 setting

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+// import { HelmetProvider } from 'react-helmet-async';
 
-ReactDOM.render(
-  <HelmetProvider>
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(
+  // <HelmetProvider>
+
+  <React.StrictMode>
     <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <App />
     </BrowserRouter>
-  </HelmetProvider>
-  ,
-  document.getElementById('root')
+  </React.StrictMode>,
+
+  // </HelmetProvider>
+  // document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
